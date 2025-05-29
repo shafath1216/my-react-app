70fb77df7e1aa389d9e84b21a3fb1e2cc5afad7e
abefimrs <mail2asifsana@gmail.com>
2025-05-29 22:37:17 +0600

    Welcome to Github Action! Learn with DIPTI Batch 5!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Github Action!
+          Welcome to Github Action! Learn with DIPTI Batch 5!
         </p>
         <a
           className="App-link"
