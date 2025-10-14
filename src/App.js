ad496bf1eaa7a97f5900ed7f1fadf7256b6e0a80
abefimrs <mail2asifsana@gmail.com>
2025-10-14 19:27:21 +0600

    Deployment test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Github Action! Learn with DIPTI Batch 5!
+          Welcome to Github Action! Learn with DIPTI Batch 6!
         </p>
         <a
           className="App-link"
