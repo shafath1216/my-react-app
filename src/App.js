be75bf730b591d3c382ce38762c37607406992ba
abefimrs <me.sanaullah.asif@gmail.com>
2025-10-16 22:42:24 +0600

    AWS deployment TRY-1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Github Action! Learn with DIPTI Batch 6.7!
+          Welcome to Github Action! Learn with DIPTI Batch 6.8!
         </p>
         <a
           className="App-link"
